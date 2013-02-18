bb83edc0d8013bff78d05cff6b770ed9e9e15a28
Greg Thornton <xdissent@me.com>
2013-02-18 11:01:32 -0600

    Remove old middleware

--- lib/redmine_git_server.rb
+++ lib/redmine_git_server.rb
@@ -3,4 +3,3 @@ module RedmineGitServer; end
 require "redmine"
 require "redmine_git_server/configuration"
 require "redmine_git_server/gitolite_adapter"
-require "redmine_git_server/git_http_middleware"
